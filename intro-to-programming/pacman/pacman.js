39f88ac51431543f18e685142957f5ec7080b60e
MikhailAgudo <mikhail.agudo@gmail.com>
2021-03-09 11:39:07 +0800

    Added Level module to generate levels.

--- intro-to-programming/pacman/pacman.js
+++ intro-to-programming/pacman/pacman.js
@@ -2,15 +2,26 @@ const Level = (() => {
     let world;
 
     const initialize = (height, width) => {
-        world = []
+        world = createWorld(height, width);
     }
 
     const createWorld = (height, width) => {
+        let newWorld = [];
 
+        for ( let i = 0; i < height; i++ ) {
+            if ( i === 0 || i === (height - 1) ) {
+                newWorld.push(addRow(width, true));
+            } else {
+                newWorld.push(addRow(width, false));
+            }
+        }
+
+        return newWorld;
     }
 
     const addRow = (width, isEdge) => {
         let newRow = [];
+
         for ( let i = 0; i < width; i++ ) {
             if ( isEdge === true ) {
                 newRow.push(2);
@@ -20,9 +31,42 @@ const Level = (() => {
                 newRow.push(1);
             }
         }
+
+        return newRow;
+    }
+
+    const displayWorld = () => {
+        let output = '';
+
+        for ( let y = 0; y < world.length; y++ ) {
+            output += '<div class="row">';
+
+            for ( let x = 0; x < world[y].length; x++ ) {
+                output += displayBlock(world[y][x]);
+            }
+
+            output += '</div>';
+        }
+
+        document.getElementById('world').innerHTML = output;
+    }
+
+    const displayBlock = (type) => {
+        switch(type) {
+            case 0:
+                return '<div class="empty"></div>';
+            case 1:
+                return '<div class="coin"></div>';
+            case 2:
+                return '<div class="brick"></div>'
+        }
     }
 
     return {
-
+        initialize,
+        displayWorld
     }
 })();
+
+Level.initialize(9, 10);
+console.log(Level.displayWorld());
