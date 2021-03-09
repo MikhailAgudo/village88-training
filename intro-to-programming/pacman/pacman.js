50dc90a374cb430d0561713719cfc219d6afeb37
MikhailAgudo <mikhail.agudo@gmail.com>
2021-03-09 15:52:58 +0800

    Added collisions and pacman movement.

--- intro-to-programming/pacman/pacman.js
+++ intro-to-programming/pacman/pacman.js
@@ -1,4 +1,4 @@
-const Entity = (x, y, classes) => {
+const Entity = (x, y, type, classes) => {
     const WIDTH = '20px';
     const HEIGHT = '20px'; // should factor in collision
     let position = {
@@ -6,13 +6,8 @@ const Entity = (x, y, classes) => {
         y: y
     }
     let property = document.createElement('div');
-
-    const initialize = () => {
-        property.style.width = WIDTH;
-        property.style.height = HEIGHT;
-
-        addClasses();
-    }
+    property.style.width = WIDTH;
+    property.style.height = HEIGHT;
 
     const addClasses = () => {
         property.classList.add(classes.shift());
@@ -22,56 +17,75 @@ const Entity = (x, y, classes) => {
         }
     }
 
+    const movePosition = (moveX, moveY) => {
+        position.x += moveX;
+        position.y += moveY;
+    }
+
     const getProperty = () => {
         return property;
     }
 
     const getPosition = () => {
-        return {x, y};
+        return position;
     }
 
-    const setX = () => {
-
-    }
+    addClasses();
 
     return {
-        initialize,
         getProperty,
-        getPosition
+        movePosition,
+        position,
+        property,
+        type
     }
 }
 
 const Pacman = (() => {
-    let pacman = document.getElementById('pacman');
-    let x = 10;
-    let y = 10;
+    let character = Entity(10, 10, 'player', [
+        'pacman',
+        'entity'
+    ])
+
+    const SPEED = 5;
+    let movement = {
+        x: SPEED,
+        y: 0
+    }
 
     const display = () => {
-        pacman.style.top = y + 'px';
-        pacman.style.left = x + 'px';
+        character.style.top = y + 'px';
+        character.style.left = x + 'px';
     }
 
-    document.onkeydown = (e) => {
-        switch (e.keyCode) {
-            case 37:
-                x--;
-                break;
-            case 38:
-                y--;
-                break;
-            case 39:
-                x++;
-                break;
-            case 40:
-                y++;
-                break;
+    const controlsGame = () => {
+        document.onkeydown = (e) => {
+            switch (e.keyCode) {
+                case 37:
+                    movement.x = -SPEED;
+                    movement.y = 0;
+                    break;
+                case 38:
+                    movement.y = -SPEED;
+                    movement.x = 0;
+                    break;
+                case 39:
+                    movement.x = SPEED;
+                    movement.y = 0;
+                    break;
+                case 40:
+                    movement.y = SPEED;
+                    movement.x = 0;
+                    break;
+            }
+            console.log(e.keyCode);
         }
-
-        display();
-        console.log(e.keyCode);
     }
+
     return {
-        display
+        character,
+        movement,
+        controlsGame
     }
 })();
 
@@ -146,9 +160,96 @@ const Level = (() => {
 })();
 
 const Game = (() => {
+    const SIZE = 20;
+    let score = 0;
+    let entities = [];
+    let gameplay = false;
+    let pacman = Pacman.character;
+    let world = document.querySelector('.world');
 
+    const start = () => {
+        gameplay = true;
+        entities.push(pacman);
+
+        let wall = Entity(50, 50, 'brick', [
+            'entity',
+            'brick'
+        ]);
+
+        entities.push(wall);
+
+        Pacman.controlsGame();
+        setInterval(loop, 33);
+    }
+
+    const loop = () => {
+        console.log('tick');
+        update();
+        display();
+    }
+
+    const update = () => {
+        let pacmanCollision = checkPacmanCollision(pacman, Pacman.movement);
+        if ( pacmanCollision === 'coin' || pacmanCollision === false ) {
+            pacman.movePosition(Pacman.movement.x, Pacman.movement.y);
+        }
+
+        for ( let i = 0; i < entities.length; i++ ) {
+            if ( checkCollision(entities[i]) === true || entities[i].type === 'coin' ) {
+                score++;
+                entities.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+    const checkCollision = (entity) => {
+        for ( let i = 0; i < entities.length; i++ ) {
+            if ( entity !== entities[i] ) {
+                //let collisi
+                if ( entity.position.x < entities[i].position.x + SIZE &&
+                    (entity.position.x + SIZE) > entities[i].position.x && 
+                    entity.position.y < entities[i].position.y + SIZE &&
+                    (entity.position.y + SIZE) > entities[i].position.y ) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    const checkPacmanCollision = (entity, movement) => {
+        for ( let i = 0; i < entities.length; i++ ) {
+            if ( entity !== entities[i] ) {
+                //let collisi
+                if ( ((entity.position.x + movement.x) < entities[i].position.x + SIZE &&
+                    (entity.position.x + movement.x + SIZE) > entities[i].position.x) && 
+                    ((entity.position.y + movement.y) < entities[i].position.y + SIZE &&
+                    (entity.position.y + movement.y + SIZE) > entities[i].position.y) ) {
+                    return entities[i].type;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    const display = () => {
+        world.innerHTML = '';
+        for ( let i = 0; i < entities.length; i++ ) {
+            let currentEntity = entities[i];
+            world.appendChild(currentEntity.property);
+            currentEntity.property.style.top = currentEntity.position.y;
+            currentEntity.property.style.left = currentEntity.position.x;
+        }
+    }
+    return {
+        start
+    }
 })();
 
-Pacman.display();
-Level.initialize(9, 10);
-console.log(Level.displayWorld());
+
+Game.start();
+//Level.initialize(9, 10);
+//console.log(Level.displayWorld());
